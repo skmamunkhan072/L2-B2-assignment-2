bdfc8b8e565472302695aa24d24f47dc55a7d5d4
Sk Mamun Khan <skmamunkhan072@gmail.com>
2023-11-25 21:34:51 +0600

    tet

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -45,7 +45,10 @@ const getSingleUser = async (req: Request, res: Response) => {
     res.status(400).json({
       success: false,
       message: err.message,
-      error: err,
+      error: {
+        code: 404,
+        description: err,
+      },
     })
   }
 }
